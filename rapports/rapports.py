92a7e88cc26f1c10c3190999a1c3647eb4234de8
ColletSimon <simoncollet2005@gmail.com>
2025-03-26 13:50:03 +0100

    creation du service rapports

--- rapports/rapports.py
+++ rapports/rapports.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import json, requests
 from pathlib import Path
+from fpdf import FPDF, HTMLMixin
 
 app = Flask(__name__)
 
@@ -8,20 +9,23 @@ sbom = {}
 vul = []
 prj = {}
 cwd= Path.cwd().joinpath("rapports")
+
+
+
 def recup_sbom(id):
     global sbom
     #requests.get("sbom/"+id)
-    with open(cwd.joinpath("exemple_sbom.json")) as f:
+    with open(cwd.joinpath("exemple_sbom.json"),encoding='UTF-8') as f:
         sbom = json.load(f)
 
 def recup_vul(id):
     global vul
-    with open(cwd.joinpath("exemple_vulnerabilite.json"))as f:
+    with open(cwd.joinpath("exemple_vulnerabilite.json"),encoding='UTF-8')as f:
         vul = json.load(f)
 
 def recup_prj(id):
     global prj
-    with open(cwd.joinpath("exemple_projet.json")) as f:
+    with open(cwd.joinpath("exemple_projet.json"),encoding='UTF-8') as f:
         prj = json.load(f)
 
 def recup_global(id):
@@ -32,16 +36,47 @@ def recup_global(id):
 @app.route("/")
 def index():
     recup_global(10)
-    return  f'''
+    return render_template("rapport.j2",projet=prj,sbom=sbom,vul=vul)
 
-<h1>{prj}</1>
-<br>
-<br>
-<h2>{sbom}</h2>
+@app.route("/pdf/<id>")
+def pdf(id):
+    recup_global(id)
 
-<br>
-<br>
-<h2>{vul}</h2>
 
-'''
-app.run()
+    pdf=FPDF()
+    pdf.add_page()
+
+    pdf.set_font("Arial", size=30)
+    pdf.cell(0,30,prj["nom"],align="C",ln=1)
+
+    pdf.set_font("Arial", size=20)
+    pdf.cell(0,20,"description : ",ln=1)
+
+    pdf.set_font("Arial", size=12)
+    pdf.cell(0,20,prj["description"],ln=1)
+
+    pdf.set_font("Arial", size=20)
+    pdf.cell(0,20,f'Version : {sbom["metadata"]["component"]["version"]}',ln=1)
+
+    pdf.cell(0,15,'Tableau de dépendances : ',ln=1)
+
+    for v in vul:
+        if str(v["id"])==id:
+
+            pdf.set_font("Arial", size=17)
+            pdf.cell(180,20,v["PkgName"]+" : "+v["InstalledVersion"],border=1,ln=1)
+
+            pdf.set_font("Arial", size=12)
+            pdf.cell(60,20,"Vulneraibilité"+" : "+v["Title"],border=1)
+            pdf.cell(60,20,"Severity"+" : "+v["Severity"],border=1)
+            pdf.cell(60,20,"Version fixé"+" : "+v["FixedVersion"],border=1,ln=1)
+
+            pdf.cell(150,20,v["Description"],border=1)
+            pdf.cell(30,20,v["References"],border=1,ln=1)
+            pdf.ln()
+
+
+    pdf.output(cwd.joinpath("mon_fichier.pdf"))
+    return send_file(cwd.joinpath("mon_fichier.pdf"))
+
+#app.run()
