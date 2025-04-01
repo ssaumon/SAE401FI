a3b2738cb1961037575441022819676f0b33c381
paulhoffbeck <paul.hoffbeck@etudiant.univ-rennes1.fr>
2025-04-01 16:57:15 +0200

    Netoyage

--- gestion-des-utilisateurs/lunsh_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-hurl --no-output test_api_bon.hurl
-
-hurl --no-output test_api_vide.hurl
-
-hurl --no-output test_api_errors.hurl
-
-# docker-compose down && docker-compose up -d --build
